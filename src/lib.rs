5f1482205b2740dfc65fb7dd8f2299b686f9b805
Brian Thuo <thuogachau@gmail.com>
2025-07-26 21:29:09 +0300

    Remove unused warning directive for non-snake_case identifiers

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-// #![warn(non_snake_case)]
 use std::fs::File;
 use std::io::{self, Read};
 
@@ -11,7 +10,6 @@ use std::io::{self, Read};
 /// # Errors
 ///
 /// Returns an `io::Error` if the file cannot be opened or read.
-
 pub fn read_file_to_string(file_path: &str) -> io::Result<String> {
     let mut f = File::open(file_path)?;
     let mut contents = String::new();

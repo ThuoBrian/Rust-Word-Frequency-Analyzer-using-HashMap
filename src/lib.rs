c0512d4aa48f0e46905fbbb8e19ea9833f72b560
Brian Thuo <thuogachau@gmail.com>
2025-07-26 22:27:47 +0300

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,7 @@ use std::io::{self, Read};
 ///
 /// * `file_path` - A string slice that holds the path of the file to open.
 ///
-/// # Errors
-///
-/// Returns an `io::Error` if the file cannot be opened or read.
+
 pub fn read_file_to_string(file_path: &str) -> io::Result<String> {
     let mut f = File::open(file_path)?;
     let mut contents = String::new();
